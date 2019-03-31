e1da589a0f224ad7f2d99701fbceb5503066a17b
anma4475 <anma4475@linux2.mif>
2019-03-31 23:07:58 +0300

    Initial

---
+++ inserts.sql
@@ -0,0 +1,46 @@
+-- Automobiliai 
+-- Marke, Modelis, Metai, Gamintojas, Numatoma_kaina, VIN 
+
+INSERT INTO Automobilis VALUES   ('Lexus', 'IS200t', 2017, 2, 33000, 'JTJBC11A202418624' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'IS300h', 2018, 4, 36540, 'JTABC22C202462475' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'RX200t', 2018, 6, 63000, 'JGCBC11A202523424' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'GS450h', 2017, 2, 53390, 'JAQBC11A318492434' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'Corolla', 2017, 3, 18030, 'JGJHA11A204789324' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'LC500', 2019, 4, 112000, 'JCJAL13A213547633' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'RAV4', 2018, 3, 29320, 'JRVBC11C321846795' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'Land Cruiser V8', 2017, 3, 43000, 'JCRAC11A234512188' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'LS500h', 2019, 2, 123000, 'JLRAA34A894456354');
+INSERT INTO Automobilis VALUES   ('Lexus', 'LS400', 1997, 4, 11000, 'JLSBC98A544321654' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'GS300', 2004, 6, 9300, 'JGSBC11A321987521' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'Corolla', 2019, 5, 21360, 'JTLCT14A287934168' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'IS200t', 2019, 4, 39350, 'JASBC68A214835276' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'Land Cruiser', 2020, 1, 48630, 'JWABC66A218976540' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'C-HR', 2018, 3, 28030, 'JAWBH24A876354298' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'IS200t', 2019, 2, 41530, 'JISAW68A111576852' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'RX450h', 2018, 6, 58360, 'JRXBC45A254830711' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'RAV4', 2019, 5, 26540, 'JASBC12C235790123' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'ES300h', 2019, 6, 63050, 'JESBC61D015879632' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'GS450h', 2007, 4, 13390, 'JCCBC11G258755113' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'Corolla', 2019, 1, 19330, 'JTCHW13A695872315' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'LC500h', 2018, 4, 122000, 'JALAL66A369875001' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'RAV4', 2011, 1, 12320, 'JRABC66G359001472' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'Land Cruiser V8', 2018, 5, 43600, 'JWLCC54A258760100' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'LS460', 2009, 4, 16360, 'JLSGS34C213548965');
+INSERT INTO Automobilis VALUES   ('Lexus', 'LS430', 2003, 5, 11400, 'JGCAC98A218463058' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'NX300h', 2018, 4, 48300, 'JSDBC33A256894520' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'C-HR', 2017, 1, 21360, 'JSLCT14A213562487' );
+INSERT INTO Automobilis VALUES   ('Lexus', 'IS200t', 2018, 6, 42350, 'JSABC68G215800311' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'Land Cruiser', 2017, 3, 38630, 'JLCBC66D258003155' );
+INSERT INTO Automobilis VALUES   ('Toyota', 'C-HR', 2020, 3, 23030, 'JGRBH24C002693102' );
+INSERT INTO Automobilis VALUES	('Lexus', 'RX200t', 2019, 4, 69530, 'JRXAW68G369021103' );
+
+
+-- Gamintojai 
+-- Gamintojo_ID, Regionas, Padalinys
+
+INSERT INTO Gamintojas VALUES   (1, 'Azija', 'Toyota');
+INSERT INTO Gamintojas VALUES   (2, 'Azija', 'Lexus');
+INSERT INTO Gamintojas VALUES   (3, 'Europa', 'Toyota');
+INSERT INTO Gamintojas VALUES   (4, 'Europa', 'Lexus');
+INSERT INTO Gamintojas VALUES   (5, 'Amerika', 'Toyota');
+INSERT INTO Gamintojas VALUES   (6, 'Amerika', 'Lexus');
