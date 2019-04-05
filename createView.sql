1937b266217681328ff8a6dca78349c77a74b672
Aurimasjar <aurimaspetretis@gmail.com>
2019-04-05 16:36:18 +0300

    Add files via upload

--- createView.sql
+++ createView.sql
@@ -7,19 +7,16 @@ WITH anma4475.VisiMokejimai AS (
 CREATE VIEW anma4475.AutomobiliaiIssimoketinai AS
 	SELECT A.VIN_numeris, D.Marke, D.Modelis, C.Vardas, C.Pavarde, B.Sumoketa_suma, A.Kaina
 	FROM anma4475.Saskaita A, anma4475.VisiMokejimai B, anma4475.Pirkejas C, anma4475.Automobilis D
-	WHERE A.VIN_numeris = D.VIN_numeris AND A.Saskaitos_ID = B.Saskaita AND A.Pirkejas = C.Pirkejo_ID
-		/*AND A.Tipas = 'Lizingas'*/;
+	WHERE A.VIN_numeris = D.VIN_numeris AND A.Saskaitos_ID = B.Saskaita AND A.Pirkejo_ID = C.Pirkejo_ID /* AND A.Tipas = 'Lizingas' */;
 		
 		
 /* Lentelė, parodanti kiekvieno pirkėjo konsultantą */	
 		
-CREATE VIEW anma4475.PirkejoKonsultantas AS
+CREATE MATERIALIZED VIEW anma4475.PirkejoKonsultantas AS
 	SELECT A.Vardas AS 'Pirkejo vardas', A.Pavarde AS 'Pirkejo pavarde',
 		B.Vardas AS 'Konsultanto vardas', B.Pavarde AS 'Konsultanto pavarde'
 	FROM Pirkejas A, Darbuotojas B
 	WHERE A.Konsultantas = B.Darbuotojo_ID
-	DATA INITIALLY REFFERED
-	REFRESH IMMEDIATE; /* Galima pridėti DATA INITIALLY DEFFERED, kada vartotojas pats pasirenka kada atnaujint */
 	
 /* Lentelė, pateikianti visus automobilius, kurie šiuo metu yra sandėlyje */
 	
@@ -33,14 +30,13 @@ CREATE VIEW anma4475.TurimiAutomobiliai AS
 CREATE VIEW anma4475.PrabangusTurimiAutomobiliai AS
 	SELECT VIN_numeris, Marke, Modelis, Metai, Numatoma_kaina
 	FROM anma4475.Automobilis
-	WHERE Statusas = 'Sandelyje' AND Numatoma_kaina > 40000;
+	WHERE Statusas = 'Sandelyje' AND Numatoma_kaina >= 40000;
 	
 /* Lentelė, pateikianti visas įmones, iš kurių superkami automobiliai */
 	
-CREATE VIEW anma4475.Partneriai AS
+CREATE MATERIALIZED VIEW anma4475.Partneriai AS
 	SELECT DISTINCT(Imone)
-	FROM anma4475.Juridinis
-	REFRESH IMMEDIATE;
+	FROM anma4475.Juridinis;
 	
 /* Lentelė, parodanti visus automobilius, esančius šiuo metu sandėlyje ir surinktus Europoje */
 	
@@ -54,5 +50,5 @@ CREATE VIEW anma4475.EuropiniaiAutomobiliai AS
 CREATE VIEW anma4475.ParduotiAutomobiliai AS
 	SELECT Saskaitos_ID, VIN_numeris, Kaina, Išrašymo_data, Pirkejo_ID, Vardas, Pavarde
 	FROM anma4475.Saskaita, anma4475.Pirkejas
-	WHERE anma4475.Saskaita.Pirkejas = anma4475.Pirkejas.Pirkejo_ID AND Tipas = 'Parduotas';
+	WHERE anma4475.Saskaita.Pirkejo_ID = anma4475.Pirkejas.Pirkejo_ID AND Tipas = 'Parduotas';
 
